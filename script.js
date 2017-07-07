300838fdbb471ffaa71d702160e783c9f454004c
yoannherlaut <yoann.herlaut@hotmail.fr>
2017-07-07 11:38:42 +0200

    modif compteur

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
 function countWord(txt){
  var splitWord = txt.split(' ');
- var countList = [];
+ var countList = {};
  for(var i =0; i<splitWord.length; i++) {
    if(splitWord[i].length >= 2) {
-
-      var isWrite = false;
-      var position;
-      for(var y=0; y<countList.length; y++) {
-       if(splitWord[i] == countList[y].word) {
-          isWrite = true;
-          position = y;
-       }
-      }
-
-      if(isWrite == false) {
-        countList.push({word: splitWord[i], count: 1});
+      if(countList[splitWord[i]] == undefined) {
+        countList[splitWord[i]] = 1;
       } else {
-        countList[position].count ++;
+        countList[splitWord[i]]++;
       }
-
    }
  }
  return countList;
